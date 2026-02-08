811e38b03fe2e52fa8187f595b0e3222b13eba68
Muhammed Korkmaz <muhammed13@gmx.de>
2026-02-08 18:39:08 +0100

    - Button Create Task creates now new list elements

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-<script>
-    let arr = [];
-</script>
+function createTask() {
+  var list = document.getElementById("listContent");
+
+  // Just pasted!!!
+  // Source - https://stackoverflow.com/a/17774064
+  // Posted by Felix Kling, modified by community. See post 'Timeline' for change history
+  // Retrieved 2026-02-07, License - CC BY-SA 3.0
+
+  var firstname = document.getElementById("listContent").value;
+  var entry = document.createElement("li");
+  entry.appendChild(document.createTextNode("listContent"));
+  list.appendChild(entry);
+}
+
+function clearList() {
+  var firstname = document.getElementById("listContent").value;
+  firstname.value.
+}
+/*
+F12 for debugging in web
+ */
